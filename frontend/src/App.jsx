375771e76e3ef51ea31924927db52c0697ca1a1b
ImSurajx <surajxcode@gmail.com>
2026-03-08 01:07:16 +0530

    feat: add application routing with react-router
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
-import Login from "./pages/AdminDashboard"
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Discover from "./pages/Discover"
+import SearchResults from "./pages/SearchResults"
+import MovieDetails from "./pages/MovieDetails"
+import Favorites from "./pages/Favorites"
+import Profile from "./pages/Profile"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
+import AdminDashboard from "./pages/admin/AdminDashboard"
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Routes>
+
+      {/* Main Pages */}
+      <Route path="/" element={<Home />} />
+      <Route path="/discover" element={<Discover />} />
+      <Route path="/search" element={<SearchResults />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/profile" element={<Profile />} />
+
+      {/* Auth */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Admin */}
+      <Route path="/admin" element={<AdminDashboard />} />
+
+    </Routes>
   )
 }
 
